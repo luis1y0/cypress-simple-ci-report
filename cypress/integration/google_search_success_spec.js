7ce3d5e441a0253d6e16042103bb0bc0dbee95ef
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2021-09-21 01:43:28 -0500

    Primer test de prueba

--- cypress/integration/google_search_success_spec.js
+++ cypress/integration/google_search_success_spec.js
@@ -1,13 +1,10 @@
-describe('Google success search', () => {
-    
-    it('https://luis1y0.github.io/challenge-frontendmentor/index.html', () => {
-        cy.visit('https://www.google.com') 
-    })
 
-    it('Search for a video', () => {
-        cy.get('[name="q"]')
-        .type('never gonna give you up youtube')
-        .should('have.value', 'never gonna give you up youtube')
-        .type('{enter}')
+describe('My First Test', function () {
+    it('Does not do much', function () {
+        cy.visit('https://luis1y0.github.io/challenge-frontendmentor/index.html')
+        cy.get('#input-bill').type('15')
+        cy.contains('15%').click()
+        cy.get('#input-people').type('1')
     })
-  })
+})
+
